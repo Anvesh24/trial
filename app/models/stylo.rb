23a3637462e2492a640b139514bcdb169ae0383e
Anvesh <anvesh.v@gmail.com>
2015-07-30 20:27:14 +0530

    cloned

--- app/models/stylo.rb
+++ app/models/stylo.rb
@@ -1,4 +1,5 @@
 class Stylo < ActiveRecord::Base
+  acts_as_votable
   belongs_to :user
   mount_uploader :photo, PhotoUploader
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'static_pages#home'
-  resources :stylos
+
+  resources :stylos do
+    member do
+      put "like",    to: "stylos#upvote"
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
